8e86b138103254e10f50dfed763ee6f29208c661
jshprintz <jshprintz@gmail.com>
2022-07-20 22:09:26 -0700

    event listener functionality added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 console.log("Javascript works")
 
 let block = document.querySelectorAll('.block');
+let board = document.getElementById('board');
 
 block.forEach(function(el){
     el.style.height = '30px';
     el.style.width = '30px';
-})
+    el.style.border = '5px groove silver';
+});
+
+board.addEventListener('click', render);
+
+function render(e){
+    console.log(e.target)
+}
