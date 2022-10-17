759299a2b99e3376122a39f1ef961e25f881da64
jshprintz <jshprintz@gmail.com>
2022-10-17 12:35:39 -0700

    sometimes bottom row produces an error. Computer clear will automatically clear a mine. Trying to figure out why.

--- js/app.js
+++ js/app.js
@@ -210,8 +210,9 @@ function notMineOrFlag(id, num) {
   if (
     squareArray[id + num].mine === false &&
     squareArray[id + num].flagged === false
-  )
+  ) {
     goodToClear = true;
+  }
 
   return goodToClear;
 }
@@ -220,7 +221,6 @@ function notMineOrFlag(id, num) {
 // Computer clears adjacent free squares
 //-----------------------------------------------------
 function computerClear(id) {
-  let testCorner = true;
   // Determine what blocks, if any, that surround the
   // current selection should be cleared by the
   // computer.
@@ -1153,9 +1153,7 @@ function casualTeachFour() {
   playerMusic.play();
 }
 
-
 function survivorTeachOne() {
-  
   changeMode();
   modeSelectEl.removeEventListener("click", changeMode);
   if (modeSelectEl.innerText === "CASUAL") {
